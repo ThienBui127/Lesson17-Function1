b8be6dfbfc8981992105d6e54cb9eca1de2b7533
ThienBui127 <126744003+ThienBui127@users.noreply.github.com>
2023-06-28 11:31:52 +0700

    goiham

---
+++ demo2.js
@@ -0,0 +1,69 @@
+// function square(number) {
+//     var result = number * number;
+//     return result;
+// }
+// var square0f2 = square(2);
+// document.write("square of 2 is: " +square0f2 );
+// document.write("<br>");
+
+// var square0f5 = square(5);
+// document.write("square of 5 is: " +square0f5 );
+
+
+// function calculate() {
+//     var input = document.getElementById("input").value;
+//     var result = square(input);
+//     document.getElementById("result").innerHTML= result;
+// }
+
+// function addNumbers(){
+//     firstNum = 4;
+//     secondNum = 8;
+//     result = firstNum + secondNum;
+//     return result;
+// }
+// result = 0;
+// sum = addNumbers();
+// alert(result);
+
+// function multiplyNums(x) {
+//     return x * 2;
+// }
+// let theNumber = 10;
+// let result = multiplyNums(theNumber);
+// alert(result);
+
+// function myFunction(x) {
+//     if (x == 1){
+//         return true;
+//     }else {
+//         return false;
+//     }         
+    
+// } 
+// myFunction();
+
+// function myFunction(){
+//     let count = 0;
+//     let firstNum = 48;
+//     return;
+//     var secondNum = 109;
+// }
+
+// function orderFruit() {
+//     let total = 0;
+//     // Call another function to place order
+//     return total;
+// }
+
+// let result = orderFruit();
+//      alert("The total is " + result);
+
+function square(x) {
+    x = x * x;
+}
+let y = 10;
+square(y);
+console.log(y);
+
+
